fe3d787812a7a021271c7f59643d93b34854cc5c
Horpeyemi <Opeyemi5055@yahoo.com>
2024-07-16 14:44:53 +0100

    Deployed

--- engrAustin_schedule/public/js/bundle.js
+++ engrAustin_schedule/public/js/bundle.js
@@ -12759,7 +12759,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62759" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63633" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
